66238703ee738b9412651d25d2d3da0c69ebbcb2
Aleksandr Barmin <abarmin@outlook.com>
2022-04-23 15:08:26 +0100

    Initial commit of the first example

---
+++ examples/example-basic/src/main/java/dev/abarmin/graalvm/HelloWorldApplication.java
@@ -0,0 +1,10 @@
+package dev.abarmin.graalvm;
+
+/**
+ * @author Aleksandr Barmin
+ */
+public class HelloWorldApplication {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
