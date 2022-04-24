1f200dbd88aed2bb7144e202d5aa8cbbe23ac916
Aleksandr Barmin <abarmin@outlook.com>
2022-04-24 18:10:20 +0100

    Regenerating project from the proper archetype

---
+++ examples/example-reflection/src/test/java/dev/abarmin/graalvm/AppTest.java
@@ -0,0 +1,20 @@
+package dev.abarmin.graalvm;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
